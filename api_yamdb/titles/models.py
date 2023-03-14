dc8ff7825a146da75c01c908056662079ef407ac
 Давид Хаишбашев <khaishbashev.david@gmail.com>
2023-03-14 21:55:13 +0300

    The first model review

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
+from datetime import datetime
 
-# Create your models here.
+class Сategories(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=256)
+    slug = models.SlugField(verbose_name='Слаг', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    
+
+class Genres(models.Model):
+    name = models.CharField(verbose_name='Название', max_length=256)
+    slug = models.SlugField(verbose_name='Слаг', max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+        ordering = ['name']
+    
+    
+class Titles(models.Model):
+    name =  models.CharField(verbose_name='Название', max_length=200)
+    description = models.TextField(null=True, blank=True)
+
+    category = models.ForeignKey(Сategories, on_delete = models.SET_NULL, null = True,
+        related_name='category')
+    genre = models.ForeignKey(Genres, on_delete = models.SET_NULL, null = True,
+        related_name='genre')
+    year = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(datetime.now().year)])
+
+    def __str__(self):
+        return self.name
+    
+    
--- api_yamdb/titles/views.py
+++ api_yamdb/titles/views.py
@@ -1,3 +1,2 @@
-from django.shortcuts import render
-
+from rest_framework import mixins
 # Create your views here.
