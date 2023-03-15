b89a6246eab8c9bb4d798020052ef7ef55e43687
Pogrem <0961@list.ru>
2023-03-15 13:23:05 +0300

    Signup

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, status, permissions
 from rest_framework.response import Response
-# from rest_framework.pagination import LimitOffsetPagination
 from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
 User = get_user_model()
 
 from .serializers import (
@@ -14,4 +14,17 @@ class SignupViewSet(viewsets.ModelViewSet):
     serializer_class = SignupSerializer
     permission_classes = (permissions.AllowAny,)
 
+    def create(self, request, *args, **kwargs):
+        serializer = SignupSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, created = User.objects.get_or_create(**serializer.validated_data)
+        email = user.email
+        send_mail(
+            'Подтверждение регистрации api_yamdb',
+            'Для подтверждение регистрации отправьте код',
+            'api_yamdb@api_yamdb.com',
+            [f'{email}'],
+            fail_silently=False,
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
