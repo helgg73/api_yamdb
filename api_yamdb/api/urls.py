816560aabc0a5e9aa18d617cdcbee565b8768853
 Давид Хаишбашев <khaishbashev.david@gmail.com>
2023-03-17 00:39:00 +0300

    Views,ursl,Serializers of Category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 from .views import signup, checktoken
+from rest_framework import routers
+from .views import CategoryViewSet
 
+
+router = routers.DefaultRouter()
+router.register(r'categories', CategoryViewSet)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
     path('v1/auth/signup/', signup, name="signups"),
     path('v1/auth/token/', checktoken, name="checktokens")
 ]
