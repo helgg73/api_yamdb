b9bdb2c7606c30da82d06935075183e4d097c740
Олег Дядьков <dyadkov@gmail.com>
2023-03-19 02:44:46 +0500

    Валидация вручную

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 
 from users.models import User
 from titles.models import Categories, Titles, Genres
@@ -104,26 +106,26 @@ class UserEditSerializer(UserSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    requires_context = True
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True,
         default=serializers.CurrentUserDefault(),
     )
-    title = serializers.SlugRelatedField(
-        slug_field='name',
-        read_only=True
-    )
     score = serializers.IntegerField(
         validators=(score_validator,)
     )
 
+    def validate(self, data):
+        request = self.context["request"]
+        if request.method == "POST":
+            author = request.user
+            title_id = self.context["view"].kwargs.get("title_id")
+            title = get_object_or_404(Titles, pk=title_id)
+            if Reviews.objects.filter(title=title, author=author).exists():
+                raise ValidationError("На произведение разрешен один отзыв")
+        return data
+
     class Meta:
-        fields = '__all__'
+        fields = ("id", "text", "author", "score", "pub_date")
         model = Reviews
-        validators = (
-            UniqueTogetherValidator(
-                queryset=Reviews.objects.all(),
-                message = 'Можно оставить только один отзыв',
-                fields=('author', 'title')
-            ),
-        )
