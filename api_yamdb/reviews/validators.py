4c6c08626a7bb73fba87aaf5aa9597dd06c78616
Олег Дядьков <dyadkov@gmail.com>
2023-03-20 11:22:11 +0500

    fix format after flake8

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -7,6 +7,7 @@ def score_validator(value):
         raise ValidationError('Оценка должна быть от 1 до 10')
     return value
 
+
 def year_validator(value):
     if value < 0 or value > datetime.now().year:
         raise ValidationError('Год должен быть от 0 до текущего')
