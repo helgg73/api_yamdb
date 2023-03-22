55de9ff0d7f727a64ef685813510d87ba8547a9a
Олег Дядьков <dyadkov@gmail.com>
2023-03-22 19:20:53 +0500

    Chane is_admin

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -45,7 +45,8 @@ class User(AbstractUser):
     @property
     def is_admin(self):
         return (
-            self.role == ADMIN or self.is_superuser or self.is_staff
+            # self.role == ADMIN or self.is_superuser or self.is_staff
+            self.role == ADMIN or self.is_staff
         )
 
     def __str__(self):
